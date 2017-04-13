64410b29dda131cdf6cffbd4ad409480dd20bd57
John W. Fawcett <jfawcett@microsoft.com>
2017-04-13 15:56:41 -0700

    Update runonce.ps1
    Changed host address

--- runonce.ps1
+++ runonce.ps1
@@ -28,7 +28,7 @@ function phoneHome($m) {
     $c = $linuxInfo.ID
     $c=$c -replace '"',""
 
-    invoke-command -Credential $cred -ComputerName 10.123.175.125 -Authentication Basic -ScriptBlock ${function:callItIn} -ArgumentList $c,$m
+    invoke-command -Credential $cred -ComputerName MSLK-BOOT-TEST-HOST.redmond.corp.microsoft.com -Authentication Basic -ScriptBlock ${function:callItIn} -ArgumentList $c,$m
 }
 
 #
