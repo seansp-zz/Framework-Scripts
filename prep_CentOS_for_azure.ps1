9669455744b8e0b480bbc53b4f998388bec4f702
John W. Fawcett <jfawcett@microsoft.com>
2017-05-22 19:47:24 -0400

    Trying to get ifcfg right

--- prep_CentOS_for_azure.ps1
+++ prep_CentOS_for_azure.ps1
@@ -15,12 +15,15 @@ function setConfig( $file, $key, $value ) {
     }
 }
 
+echo "setting network script"
 setConfig "/etc/sysconfig/network" "NETWORKING" "yes" 
 setConfig "/etc/sysconfig/network" "HOSTNAME" "localhost.localdomain" 
 
+echo "Removing old ifcfg script"
 remove-item -force /etc/sysconfig/network-scripts/ifcfg-eth0
 
-echo "
+echo "setting up new ifcfg script"
+echo '
 DEVICE=eth0
 ONBOOT=yes
 BOOTPROTO=dhcp
@@ -28,8 +31,9 @@ TYPE=Ethernet
 USERCTL=no
 PEERDNS=yes
 IPV6INIT=no
-NM_CONTROLLED=no" | set-content -encoding UTF8 /etc/sysconfig/network-scripts/ifcfg-eth0
+NM_CONTROLLED=no' | set-content -encoding UTF8 /etc/sysconfig/network-scripts/ifcfg-eth0
 
+echo "Linking the rules"
 ln -s /dev/null /etc/udev/rules.d/75-persistent-net-generator.rules
 
 #
@@ -37,6 +41,7 @@ ln -s /dev/null /etc/udev/rules.d/75-persistent-net-generator.rules
 #
 #  Get the existing command line
 #
+echo "Fixing GRUB"
 $grubLine=(sls  'GRUB_CMDLINE_LINUX' /etc/default/grub | select -exp line)
 
 #
@@ -60,8 +65,10 @@ $grubLine=$grubLine -replace '"$',' rootdelay=300 console=ttyS0 earlyprintk=ttyS
 #
 (Get-Content /etc/default/grub) -replace 'GRUB_CMDLINE_LINUX=.*',$grubLine | Set-Content -encoding UTF8 /etc/default/grub
 
+echo "Setting up new GRUB"
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
+echo "Installing Python and WAAgent"
 curl -o /etc/yum.repos.d/openlogic.repo https://raw.githubusercontent.com/szarkos/AzureBuildCentOS/master/config/azure/OpenLogic.repo
 curl -o /etc/pki/rpm-gpg/OpenLogic-GPG-KEY https://raw.githubusercontent.com/szarkos/AzureBuildCentOS/master/config/OpenLogic-GPG-KEY
 
@@ -74,6 +81,7 @@ setConfig "/etc/waagent.conf" "ResourceDisk.MountPoint" "/mnt/resource"
 setConfig "/etc/waagent.conf" "ResourceDisk.EnableSwap" "y" 
 setConfig "/etc/waagent.conf" "ResourceDisk.SwapSizeMB" "2048" 
 
+echo "Deprovisioning..."
 # waagent -force -deprovision
 # exit
 
