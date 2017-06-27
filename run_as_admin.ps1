1581dea0e21aaee2e122812a985f52ee8b334296
John Fawcett <jfawcett@microsoft.com>
2017-06-27 10:47:56 -0700

    Hih?

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -7,6 +7,7 @@ $pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
 $cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
 
 $command = $PSScriptRoot + "/" + $script
+$command
 invoke-command $command -ComputerName localhost  -Credential $cred
 
 Wait-Process -Id $process.Id
