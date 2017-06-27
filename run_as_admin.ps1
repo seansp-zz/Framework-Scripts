063a35186c70ac6d590a709bb5f69b84fa029c4c
John Fawcett <jfawcett@microsoft.com>
2017-06-27 07:14:46 -0700

    Trying to get around Windows Security

---
+++ run_as_admin.ps1
@@ -0,0 +1,9 @@
+﻿param (
+    [Parameter(Mandatory=$true)] [string] $scriptName
+)
+
+$o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
+$pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
+$cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
+
+Start-Process -Wait -Credential $cred -FilePath powershell.exe -ArgumentList $scriptName -Verbose
