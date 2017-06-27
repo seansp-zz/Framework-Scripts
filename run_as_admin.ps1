4f5befe8115766dd19dfa3603e50020626949993
John Fawcett <jfawcett@microsoft.com>
2017-06-27 07:33:45 -0700

    More

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -2,9 +2,9 @@
     [Parameter(Mandatory=$true)] [string] $scriptName
 )
 
-# $o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
-# $pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
-# $cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
+$o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
+$pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
+$cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
  
-$process = Start-Process -Wait -FilePath powershell.exe -ArgumentList $scriptName -Verbose -NoNewWindow
+$process = Start-Process -Wait -Credential $cred -FilePath powershell.exe -ArgumentList $scriptName -Verbose -NoNewWindow
 Wait-Process -Id $process.Id
