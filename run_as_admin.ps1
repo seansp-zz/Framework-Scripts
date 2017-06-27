0c0fbfa673bd57f2dc7787e5925b40d96f4b2c18
John Fawcett <jfawcett@microsoft.com>
2017-06-27 10:46:00 -0700

    Again

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -6,6 +6,7 @@ $o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
 $pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
 $cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
 
-invoke-command $script -ComputerName localhost  -Credential $cred
+$command = $PSScriptRoot + "/" + $script
+invoke-command $command -ComputerName localhost  -Credential $cred
 
 Wait-Process -Id $process.Id
