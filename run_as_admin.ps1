b2905ac2f7714ed8d11f4ae45eb8a8e270889c26
John Fawcett <jfawcett@microsoft.com>
2017-06-27 07:23:03 -0700

    Wait for the process to end

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -6,4 +6,5 @@ $o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
 $pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
 $cred=new-object -typename system.management.automation.pscredential -argumentlist "MSTest",$pw
  
-Start-Process -Wait -Credential $cred -FilePath powershell.exe -ArgumentList $scriptName -Verbose -NoNewWindow
+$process = Start-Process -Wait -Credential $cred -FilePath powershell.exe -ArgumentList $scriptName -Verbose -NoNewWindow
+Wait-Process -Id $process
