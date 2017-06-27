06a8ba0e46a0aff4c12173da5d89821d48b332e6
John Fawcett <jfawcett@microsoft.com>
2017-06-27 07:53:53 -0700

    Trying local domain

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -4,7 +4,7 @@
 
 $o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
 $pw=convertto-securestring -AsPlainText -force -string 'P@$$w0rd!'
-$cred=new-object -typename system.management.automation.pscredential -argumentlist "\MSTest",$pw
+$cred=new-object -typename system.management.automation.pscredential -argumentlist "lis-f1637\MSTest",$pw
  
 $process = Start-Process -Wait -Credential $cred -FilePath powershell.exe -ArgumentList $scriptName -Verbose -NoNewWindow
 Wait-Process -Id $process.Id
