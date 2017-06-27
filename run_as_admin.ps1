62068922bcc8aed40ce2cbd4131f31229c6566b5
John Fawcett <jfawcett@microsoft.com>
2017-06-27 14:15:42 -0700

    Still trying

--- run_as_admin.ps1
+++ run_as_admin.ps1
@@ -8,11 +8,9 @@ $cred=new-object -typename system.management.automation.pscredential -argumentli
 
 $s=New-PSSession -ComputerName 169.254.241.55 -Authentication Basic -Credential $cred  -Port 443 -UseSSL -SessionOption $o
 
-[string]$scripttext="{ ./run_as_admin.ps1 " + $script + '; $cmd_status=$?' + " }"
-[ScriptBlock]$scriptblock = [ScriptBlock]::Create($scripttext)
+invoke-command -session $s -FilePath ./run_as_admin.ps1 -ArgumentList $script
 
-invoke-command -session $s -ScriptBlock $scriptblock
-
-$remote_status = invoke-command -Session $s -ScriptBlock { $cmd_status }
+$remote_status = invoke-command -Session $s -ScriptBlock { $? }
+$remote_status
 
 exit $remote_status
