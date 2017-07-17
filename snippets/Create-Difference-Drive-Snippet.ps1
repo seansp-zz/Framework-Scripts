f398b2d0b9316926049ce634575ef8ec3134f66e
Sean Spratt <seansp@gmail.com>
2017-07-17 10:19:36 -0700

    Added two snippets from noktpapilio.
    These two powershell scripts are how I am both creating Difference Drives (with VHDs) and the copy process I am using for working files.
    
    Presently, when I receive a VHD, I make a difference.  Then I use my $flavor to duplicate the delta as a working file which I use to test my setup script.

---
+++ snippets/Create-Difference-Drive-Snippet.ps1
@@ -0,0 +1,4 @@
+﻿$parent = "C:\Users\seansp\Desktop\azure_images\original\RHEL72-Smoke-1.vhd"
+$delta = "C:\Users\seansp\Desktop\azure_images\original\RHEL72-Smoke-1-delta.vhd"
+New-VHD -ParentPath $parent -Path $delta -Differencing
+
---
+++ snippets/Prepare-Flavor-Snippet.ps1
@@ -0,0 +1,13 @@
+﻿
+$flavor = "CentOS-7.1611"
+$flavor = "RHEL72-Smoke-1"
+
+$smokeDelta = "C:\Users\seansp\Desktop\azure_images\original\$flavor-delta.vhd"
+$workingFile = "C:\Users\seansp\Desktop\azure_images\$flavor-working.vhd"
+Copy-Item -Path $smokeDelta -Destination $workingFile -Force
+$vm = New-VM -Name $flavor -VHDPath $workingFile -MemoryStartupBytes 7168mb -Generation 1 -SwitchName "External-WIFI"
+start-vm -VM $vm
+
+
+# copy delta drive to new file.
+#new-vm -Name "SBI Smoke 1" -MemoryStartupBytes 7168mb -Generation 1 -SwitchName "External" -VHDPath $originalDrive > $null
