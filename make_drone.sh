9524029279d5243495fc6aa7e368b2d595e1b736
John Fawcett <jfawcett@microsoft.com>
2017-07-24 14:03:36 -0700

    Ignore errors on developer tools install

--- make_drone.sh
+++ make_drone.sh
@@ -170,7 +170,7 @@ yum -y install wget
 rpm -Uvh http://linux.mirrors.es.net/fedora-epel/7/x86_64/i/iperf-2.0.8-1.el7.x86_64.rpm
 yum -y localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
 yum -y install mysql-community-server
-yum -y groupinstall "Development Tools"
+yum -y groupinstall --skip-broken "Development Tools"
 yum -y install bind bind-utils
 yum -y install python python-pyasn1
 yum -y install python-argparse
