4b252749bea96af81c0c26c2ead5c0e62b410c77
John Fawcett <jfawcett@microsoft.com>
2017-07-27 14:35:06 -0700

    More

--- create_drone_from_container_vhd.ps1
+++ create_drone_from_container_vhd.ps1
@@ -161,7 +161,7 @@ $scriptBlockString =
     #
     #  The first one gets the machine added to known_hosts
     Write-Host "Copying make_drone to the target.." -ForegroundColor Green
-    echo "y" | C:\azure-linux-automation\tools\pscp -batch C:\temp\make_drone.sh $username@$ip`:/tmp
+    echo "y" | C:\azure-linux-automation\tools\pscp C:\temp\nix_files\make_drone.sh $username@$ip`:/tmp
 
     #
     #  Now transfer the files
