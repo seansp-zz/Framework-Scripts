80ee4215052427d7a8d58dec5c1bbe3c21823e66
John Fawcett <jfawcett@microsoft.com>
2017-07-27 08:14:11 -0700

    trying to get all the nodes to start

--- create_vhd_from_urn.ps1
+++ create_vhd_from_urn.ps1
@@ -15,15 +15,15 @@
 )
 write-host "The array is $vmNames"
 $vmNameArray = $vmNames.Split(",")
-$vmNameArray
+$blobURNArray = $blobURNArray.Split(",")
 
-if ($vmNames.Count -ne $blobURNs.Count) {
+if ($vmNameArray.Count -ne $blobURNs.Count) {
     Write-Host "Please procvide the same number of names and URNs."
     exit 1
 } else {
     Write-Host "There are $vmNames.Count() left..."
 }
-
+$vmName = $vmNames[0]
 Start-Transcript C:\temp\transcripts\create_vhd_from_urn_$vmNames[0].log
 
 . "C:\Framework-Scripts\common_functions.ps1"
@@ -48,31 +48,42 @@ $vnetSubnetAddressPrefix = "10.0.0.0/24"
 
 $vmSize = "Standard_A2"
 
-Write-Host "Stopping any running VMs" -ForegroundColor Green
-Get-AzureRmVm -ResourceGroupName $destRG -status | Where-Object -Property Name -Like "$vmName*" | where-object -Property PowerState -eq -value "VM running" | Stop-AzureRmVM -Force
+#
+#  Yes, these are done sequentially, not in parallel.  I will figure that out later :)
+#
+$i = 0
+while ($i -lt $vmNames.Count) {
+    $vmName = $vmNames[$i]
+    $blobURN = $all_URNs[$i]
+    $i++
+    Write-Host "Preparing machine $vmName for service as a drone..."
 
-Write-Host "Clearing any old images..." -ForegroundColor Green
-Get-AzureStorageBlob -Container $destContainer -Prefix $vmName | ForEach-Object {Remove-AzureStorageBlob -Blob $_.Name -Container $destContainer}
+    Write-Host "Stopping any running VMs" -ForegroundColor Green
+    Get-AzureRmVm -ResourceGroupName $destRG -status | Where-Object -Property Name -Like "$vmName*" | where-object -Property PowerState -eq -value "VM running" | Stop-AzureRmVM -Force
 
-Write-Host "Attempting to create virtual machine $vmName.  This may take some time." -ForegroundColor Green
-Write-Host "User is $TEST_USER_ACCOUNT_NAME"
-Write-Host "Password is $TEST_USER_ACCOUNT_PAS2"
-$diskName=$vmName + $suffix
-write-host "Creating machine $vmName"
-az vm create -n $vmName -g $destRG -l $location --image $blobURN --storage-container-name $destContainer --use-unmanaged-disk --nsg $NSG `
-   --subnet $subnetName --vnet-name $vnetName  --storage-account $destSA --os-disk-name $diskName --admin-password `'$TEST_USER_ACCOUNT_PAS2`' --admin-username $TEST_USER_ACCOUNT_NAME `
-   --authentication-type "password"
+    Write-Host "Clearing any old images..." -ForegroundColor Green
+    Get-AzureStorageBlob -Container $destContainer -Prefix $vmName | ForEach-Object {Remove-AzureStorageBlob -Blob $_.Name -Container $destContainer}
 
-if ($? -eq $false) {
-    Write-Error "Failed to create VM.  Details follow..."
-    Stop-Transcript
-    exit 1
+    Write-Host "Attempting to create virtual machine $vmName.  This may take some time." -ForegroundColor Green
+    Write-Host "User is $TEST_USER_ACCOUNT_NAME"
+    Write-Host "Password is $TEST_USER_ACCOUNT_PAS2"
+    $diskName=$vmName + $suffix
+    write-host "Creating machine $vmName"
+    az vm create -n $vmName -g $destRG -l $location --image $blobURN --storage-container-name $destContainer --use-unmanaged-disk --nsg $NSG `
+       --subnet $subnetName --vnet-name $vnetName  --storage-account $destSA --os-disk-name $diskName --admin-password `'$TEST_USER_ACCOUNT_PAS2`' --admin-username $TEST_USER_ACCOUNT_NAME `
+       --authentication-type "password"
+
+    if ($? -eq $false) {
+        Write-Error "Failed to create VM.  Details follow..."
+        Stop-Transcript
+        exit 1
+    }
+    Write-Host "VM Created successfully.  Stopping it now..."
+    # Stop-AzureRmVM -ResourceGroupName $destRG -Name $diskName -Force
+
+    # Write-Host "Deleting the VM so we can harvest the VHD..."
+    # Remove-AzureRmVM -ResourceGroupName $destRG -Name $diskName -Force
+
+    Write-Host "Machine $vmName is ready for assimilation..."
 }
-Write-Host "VM Created successfully.  Stopping it now..."
-# Stop-AzureRmVM -ResourceGroupName $destRG -Name $diskName -Force
-
-# Write-Host "Deleting the VM so we can harvest the VHD..."
-# Remove-AzureRmVM -ResourceGroupName $destRG -Name $diskName -Force
-
-Write-Host "Machine is ready for assimilation..."
 Stop-Transcript
