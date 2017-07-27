af137ce30ba456786ac53351872ed6441e36ad0d
John Fawcett <jfawcett@microsoft.com>
2017-07-27 12:25:53 -0700

    More

--- create_drone_from_container_vhd.ps1
+++ create_drone_from_container_vhd.ps1
@@ -62,8 +62,7 @@ if ($makeDronesFromAll -eq $true) {
 } else {
     foreach ($vmName in $vmNameArray) {
         Write-Host "Looking at image $vmName in container $sourceContainer"
-        $theName = $vmName +"*"
-        $singleBlob=get-AzureStorageBlob -Container $sourceContainer -Blob $theName -ErrorAction SilentlyContinue
+        $singleBlob=get-AzureStorageBlob -Container $sourceContainer -Blob "$vmName*.vhd" -ErrorAction SilentlyContinue
         if ($? -eq $true) {
             $copyblobs.add($singleBlob)
         } else {
