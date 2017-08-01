a7a81fc90c663c5a9fcc424d1c2a5b508bc2d384
John Fawcett <jfawcett@microsoft.com>
2017-08-01 15:04:14 -0700

    Forgot an important one

--- common_functions.ps1
+++ common_functions.ps1
@@ -73,3 +73,41 @@ function remove_machines_from_group([string[]] $runningVMs,
         }
     }
 }
+
+function deallocate_machines_in_group([string[]] $runningVMs,
+                                      [string] $destRG)
+{
+    foreach ($vm_name in $runningVMs) {
+        $vmJobName = $vm_name + "-Src"
+        Start-Job -Name $vmJobName -ScriptBlock {Stop-AzureRmVM -Name $vm_name -ResourceGroupName $destRG -Force}
+    }
+
+    $allDone = $false
+    while ($allDone -eq $false) {
+        $allDone = true
+        foreach ($vm_name in $runningVMs) {
+            $vmJobName = $vm_name + "-Src"
+            $jobStat = Get-Job -Name $vmJobName
+            $jobState = $job.State
+            write-host "    Job $vmJobName is in state $jobState" -ForegroundColor Yellow
+            if ($jobState -eq "Running") {
+                $allDone = $false
+            }
+        }
+
+        foreach ($vm_name in $runningVMsDest) {
+            $vmJobName = $vm_name + "-Dest"
+            $jobStat = Get-Job -Name $vmJobName
+            $jobState = $job.State
+            write-host "    Job $vmJobName is in state $jobState" -ForegroundColor Yellow
+            if ($jobState -eq "Running") {
+                $allDone = $false
+            }
+        }
+
+        if ($allDone -eq $false) {
+            sleep(10)
+        }
+    }
+}
+
