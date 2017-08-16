06daef4fbea0de51958b26147dd3faf154fad78a
Sean Spratt <seansp@gmail.com>
2017-08-16 13:53:01 -0700

    Moved ConvertFrom-ArbritraryXml and [pester] folder added.

--- pester/ConvertFrom-ArbritraryXml.Tests.ps1
+++ pester/ConvertFrom-ArbritraryXml.Tests.ps1
@@ -1,8 +1,8 @@
-﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+﻿# $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+# . "$here\$sut"
 
-. .\ConvertFrom-ArbritraryXml.ps1
+Import-Module .\ConvertFrom-ArbritraryXml.psm1
 
 Describe "ConvertFrom-JSON -- Simple Case" {
     ####################################################################################################################
